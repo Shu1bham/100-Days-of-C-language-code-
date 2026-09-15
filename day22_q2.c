09c5c73eaf24a2004ea6203a9d03fd387d9411e9
Shubham <Simon.33377@stu.upes.ac.in>
2026-09-15 21:27:40 +0530

    Day-22 solutions

---
+++ day22_q2.c
@@ -0,0 +1,42 @@
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    double sum = 0, term;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            term = 1.0;
+        }
+        else
+        {
+            term = (double)(2 * i - 1) / (2 * i);
+        }
+        sum = sum + term;
+    }
+
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
