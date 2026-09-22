4244d878d81bd26606415d75a3bd9c1d20e6d03c
Shubham <Simon.33377@stu.upes.ac.in>
2026-09-22 14:27:01 +0530

    Day-39 solutions

---
+++ day39_q2.c
@@ -0,0 +1,34 @@
+// Q78: Find the sum of main diagonal elements for a square matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    int a[100][100];
+
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &a[i][j]);
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += a[i][i];
+
+    printf("%d\n", sum);
+
+    return 0;
+}
